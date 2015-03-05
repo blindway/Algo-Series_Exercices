f2dd0e869ad0d0f6cb6e51967057822b23de79d3
blindway <blindway@hotmail.fr>
2015-03-05 15:06:39 +0100

    S03 - 2
    ListItr- insertAfter

--- src/s03/ListItr.java
+++ src/s03/ListItr.java
@@ -1,53 +1,68 @@
 package s03;
 
 public class ListItr {
-	  List list;
-	  ListNode pred, succ;
-	  // ----------------------------------------------------------
-	  public ListItr( List anyList ) {
-	    list = anyList; 
-	    goToFirst();   
-	  }
-	  // ----------------------------------------------------------
-	  public void    insertAfter(int e) {
-	    // TODO - A COMPLETER
-	  }
-	  // ----------------------------------------------------------
-	  public void    removeAfter() {
-	    // TODO - A COMPLETER
-	  }
-	  // ----------------------------------------------------------
-	  public int  consultAfter() {
-	    return succ.elt;
-	  }
-	  public void goToNext() {
-	    pred = succ; 
-	    succ = succ.next; 
-	  }
-	  public void goToPrev() {
-	    succ = pred;
-	    pred = pred.prev; 
-	  }
-	  public void goToFirst() { 
-	    succ = list.first; 
-	    pred = null;
-	  }
-	  public void goToLast() { 
-	    pred = list.last;  
-	    succ = null;
-	  }
-	  public boolean isFirst() { 
-	    return pred == null;
-	  }
-	  public boolean isLast() { 
-	    return succ == null; 
-	  }
-	}
+  List list;
+  ListNode pred, succ;
 
-	// When isFirst(), it is forbidden to call goToPrev()
-	// When isLast(),  it is forbidden to call goToNext() 
-	// When isLast(),  it is forbidden to call consultAfter(), or removeAfter()
-	// For an empty list, isLast()==isFirst()==true
-	// For a fresh ListItr, isFirst()==true
-	// Using multiple iterators on the same list is allowed only 
-	//   if none of them modifies the list
+  // ----------------------------------------------------------
+  public ListItr(List anyList) {
+    list = anyList;
+    goToFirst();
+  }
+
+  // ----------------------------------------------------------
+  public void insertAfter(int e) {
+    ListNode aux = new ListNode(e, pred, succ);
+    list.first = aux;
+    list.last = aux;
+    list.size++;
+    succ = aux;
+    pred = aux.prev;
+  }
+
+  // ----------------------------------------------------------
+  public void removeAfter() {
+    // TODO - A COMPLETER
+  }
+
+  // ----------------------------------------------------------
+  public int consultAfter() {
+    return succ.elt;
+  }
+
+  public void goToNext() {
+    pred = succ;
+    succ = succ.next;
+  }
+
+  public void goToPrev() {
+    succ = pred;
+    pred = pred.prev;
+  }
+
+  public void goToFirst() {
+    succ = list.first;
+    pred = null;
+  }
+
+  public void goToLast() {
+    pred = list.last;
+    succ = null;
+  }
+
+  public boolean isFirst() {
+    return pred == null;
+  }
+
+  public boolean isLast() {
+    return succ == null;
+  }
+}
+
+// When isFirst(), it is forbidden to call goToPrev()
+// When isLast(), it is forbidden to call goToNext()
+// When isLast(), it is forbidden to call consultAfter(), or removeAfter()
+// For an empty list, isLast()==isFirst()==true
+// For a fresh ListItr, isFirst()==true
+// Using multiple iterators on the same list is allowed only
+// if none of them modifies the list
