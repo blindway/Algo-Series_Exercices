ce3af7dda48e7518b9f8ee29e6f792a3572596e0
AlexTravasso <alex.travasso@gmail.com>
2015-03-09 15:47:09 +0100

    a

--- src/s03/AmStramGram.java
+++ src/s03/AmStramGram.java
@@ -1,39 +1,26 @@
 package s03;
 
 public class AmStramGram {
-
-	public static int winnerAmStramGram (int n, int k){
-		
+	public static int winnerAmStramGram(int n, int k) {
 		List l = new List();
 		ListItr li = new ListItr(l);
-			   
 		for (int i = 0; i <= n; i++) {
-			
+
 			li.insertAfter(i);
 		}
-		
 		li.goToFirst();
-	
-		
-		for (int i = k; l.size() < 1; i--) {
-			
-			li.goToFirst();
-			
-			if(li.isLast()){
+		for (int i = k; i < 1; i--) {
+			li.goToNext();
+			if (li.isLast()) {
 				li.goToFirst();
 			}
-			
+			li.removeAfter();
 		}
-		
-		
-		return 42;
+		return l.first.elt;
 	}
-	
-	
+
 	public static void main(String[] args) {
-		
-		System.out.println(winnerAmStramGram(5, 5));
+
+		System.out.println(winnerAmStramGram(5, 34));
 	}
-	
-	
 }
