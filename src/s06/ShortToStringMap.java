d9cd3dd6662cf5b3fad65f45cc5d271c697a78a4
blindway <blindway@hotmail.fr>
2015-03-30 14:53:01 +0200

    S06

--- src/s06/ShortToStringMap.java
+++ src/s06/ShortToStringMap.java
@@ -29,11 +29,11 @@ public class ShortToStringMap {
 
 	// ------------------------------------------------------------
 	public boolean containsKey(short k) {
-		if(getPosOfKey(k) != -1) return true;
+		if (getPosOfKey(k) != -1)
+			return true;
 		return false;
 	}
 
-	
 	// ------------------------------
 	// Public methods
 	// ------------------------------
@@ -88,18 +88,12 @@ public class ShortToStringMap {
 	// ------------------------------------------------------------
 	public void remove(short key) {
 		if (containsKey(key)) {
-<<<<<<< HEAD
 			int pos = getPosOfKey(key);
-			tabKey[pos] = tabKey[size-1];
-			tabValue[pos] = tabValue[size-1];
-=======
-			tabKey[getPosOfKey(key)] = tabKey[size - 1];
-			tabValue[getPosOfKey(key)] = tabValue[size - 1];
->>>>>>> origin/master
+			tabKey[pos] = tabKey[size - 1];
+			tabValue[pos] = tabValue[size - 1];
 			size--;
 		}
 	}
-
 
 	// ------------------------------------------------------------
 	public boolean isEmpty() {
@@ -114,11 +108,8 @@ public class ShortToStringMap {
 	// a.union(b) : a becomes "a union b"
 	// images are those in b whenever possible
 	public void union(ShortToStringMap m) {
-
-		for (int i = 0; i < m.tabKey.length; i++) {
-			if (!(this.containsKey((short) (m.tabKey[i])))) {
-				this.put((short) m.tabKey[i], m.tabValue[i]);
-			}
+		for (int i = 0; i < m.size; i++) {
+			this.put((short) m.tabKey[i], m.tabValue[i]);
 		}
 	}
 
@@ -126,10 +117,13 @@ public class ShortToStringMap {
 	// a.intersection(b) : "a becomes a intersection b"
 	// images are those in b
 	public void intersection(ShortToStringMap s) {
-
-		for (int i = 0; i < this.tabKey.length; i++) {
+		int posi = 0;
+		for (int i = 0; i < this.size; i++) {
 			if (s.containsKey((short) this.tabKey[i])) {
-				this.remove((short) this.tabKey[i]);
+				posi = s.getPosOfKey((short) this.tabKey[i]);
+				this.put((short) s.tabKey[posi], s.tabValue[posi]);
+			} else {
+				this.remove((short) i);
 			}
 		}
 	}
