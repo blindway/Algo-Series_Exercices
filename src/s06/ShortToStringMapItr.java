263cfa3bdb38be47c2dabe08cabb630307dfaab6
blindway <blindway@hotmail.fr>
2015-04-01 16:27:28 +0200

    S06

--- src/s06/ShortToStringMapItr.java
+++ src/s06/ShortToStringMapItr.java
@@ -1,17 +1,20 @@
 package s06;
 public class ShortToStringMapItr {
-  // TODO - A COMPLETER...
+
+	private ShortToStringMap stsm;
+	private int position = 0;
   // ----------------------------------------
  public ShortToStringMapItr (ShortToStringMap m) {
-    // TODO - A COMPLETER...
+    this.stsm = m;
   }
  // ----------------------------------------
   public boolean hasMoreKeys() {
-    return false; // TODO - A COMPLETER...
+    return position<stsm.size();
   }
   // ----------------------------------------
   // PRE-condition: hasMoreKeys()
   public short   nextKey() {
+	 // short result = (short) stsm.get(stsm.getPosOfKey((short) position));
     return 0; // TODO - A COMPLETER...
   }
 }
