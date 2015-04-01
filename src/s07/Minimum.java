7c59691e418e096e8561b67a25429364d31334b0
AlexTravasso <alex.travasso@gmail.com>
2015-04-01 16:32:21 +0200

    Minimum

---
+++ src/s07/Minimum.java
@@ -0,0 +1,26 @@
+package s07;
+
+public class Minimum {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		int tab[] = {6,1,3,3,9,7,4,2,1};
+		
+		Min(tab);
+		
+	}
+	
+	public static int Min(int[] tab){
+		
+		int min = tab[0];
+		
+		
+		
+		
+		
+		return 42;
+	}
+
+}
