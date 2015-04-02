538c5b8e64575f4155c78270b14ac4947d615ead
AlexTravasso <alex.travasso@gmail.com>
2015-04-02 16:05:34 +0200

    s07 e02

--- src/s07/Quicksort.java
+++ src/s07/Quicksort.java
@@ -1,38 +1,71 @@
 package s07;
+
 public class Quicksort {
-  public static void main(String [] args) {
-    int [] t = {4, 3, 2, 6, 8, 7};
-    int [] u = {2, 3, 4, 6, 7, 8};
-    quickSort(t);
-    for (int i=0; i<t.length; i++) 
-      if(t[i] != u[i]) {
-        System.out.println("Oups. Something is wrong...");
-        System.exit(-1);
-      }
-    System.out.println("OK. Tiny test passed...");
-  }
-//------------------------------------------------------------
-  private static int partition (int [] t, int left, int right) {
-	  //choisir le premier élément comme pivot
-	  // pour chaque case suivante i de left+1 à right{
-	  //   si l'élément courant doit aller dans la partie grisée
-	  //     agrandir la partie grisée, en échangeant les cases k+1 et i
-	  //  }
-	  //mettre le pivot à la fin de la partie grisée
-	  //retourner la poistion de l'extrémité droite de la partie grisée
-	  
-    return 0; // A COMPLETER
-  }
-//------------------------------------------------------------
-  private static void quickSort (int [] t, int left, int right) {
-    if (left>right)
-      return;
-    int p = partition(t, left, right);
-    quickSort(t, left, p-1);
-    quickSort(t, p+1, right);
-  }
-//------------------------------------------------------------
-  public static void quickSort (int [] t) {
-    quickSort(t, 0, t.length - 1);
-  }
+	public static void main(String[] args) {
+		int[] t = { 4, 3, 2, 6, 8, 7 };
+		int[] u = { 2, 3, 4, 6, 7, 8 };
+		quickSort(t);
+		for (int i = 0; i < t.length; i++)
+			if (t[i] != u[i]) {
+				System.out.println("Oups. Something is wrong...");
+				
+				for (int j = 0; j < t.length; j++) {
+					System.out.println(t[j]);
+				}
+				
+				System.exit(-1);
+			}
+		System.out.println("OK. Tiny test passed...");
+	}
+
+	// ------------------------------------------------------------
+
+	private static int partition(int[] t, int left, int right) {
+
+		int p = t[0];
+		int temp;
+		int mid = t.length / 2;
+
+		for (int i = 0; i < mid; i++) {
+			if (t[i] < p) {
+				temp = t[i];
+				t[i] = p;
+				t[i-1] = temp;
+			}
+		}
+
+		p = t[t.length - 1];
+		for (int i = t.length - 1; i > mid; i--) {
+			if (t[i] > p) {
+				temp = t[i];
+				t[i] = p;
+				t[i + 1] = temp;
+			}
+		}
+
+		return right;
+
+		// choisir le premier ï¿½lï¿½ment comme pivot
+		// pour chaque case suivante i de left+1 ï¿½ right{
+		// si l'ï¿½lï¿½ment courant doit aller dans la partie grisï¿½e
+		// agrandir la partie grisï¿½e, en ï¿½changeant les cases k+1 et i
+		// }
+		// mettre le pivot ï¿½ la fin de la partie grisï¿½e
+		// retourner la poistion de l'extrï¿½mitï¿½ droite de la partie grisï¿½e
+
+	}
+
+	// ------------------------------------------------------------
+	private static void quickSort(int[] t, int left, int right) {
+		if (left > right)
+			return;
+		int p = partition(t, left, right);
+		quickSort(t, left, p - 1);
+		quickSort(t, p + 1, right);
+	}
+
+	// ------------------------------------------------------------
+	public static void quickSort(int[] t) {
+		quickSort(t, 0, t.length - 1);
+	}
 }
