a6b00af2c42e79ed1670725ff477bbff535d93f5
AlexTravasso <alex.travasso@gmail.com>
2015-05-12 14:31:03 +0200

    KKK

--- src/s13/RndFlipCoin.java
+++ src/s13/RndFlipCoin.java
@@ -1,24 +1,30 @@
 package s13;
+
 import java.util.Random;
+
 public class RndFlipCoin {
-  public static void main(String [] args) {
-    int nbOfExperiments = 100000;
-    int n=100, m=27;
-    Random r = new Random();
-    testFlipCoin(r, m, n, nbOfExperiments);
-  }
-  // ------------------------------------------------------------
-  public static boolean flipCoin(Random r, int m, int n) {
-    return false; // TODO
-  }
-  //============================================================
-  static void testFlipCoin(Random r, int m, int n, int nbOfTests) {
-    int nbOfTrue = 0;
-    for (int i = 0; i < nbOfTests; i++) {
-      if (flipCoin(r, m, n))
-        nbOfTrue++;
-    }
-    System.out.println("There was " + nbOfTrue + " TRUE among " + nbOfTests +
-                       " flips of coin");
-  }
+	public static void main(String[] args) {
+		int nbOfExperiments = 100000;
+		int n = 100, m = 27;
+		Random r = new Random();
+		testFlipCoin(r, m, n, nbOfExperiments);
+	}
+
+	// ------------------------------------------------------------
+	public static boolean flipCoin(Random r, int m, int n) {
+		// une méthode qui tire un booléen suivant la probabilité m/n
+
+		return r.nextDouble() < (double)m/n;
+	}
+
+	// ============================================================
+	static void testFlipCoin(Random r, int m, int n, int nbOfTests) {
+		int nbOfTrue = 0;
+		for (int i = 0; i < nbOfTests; i++) {
+			if (flipCoin(r, m, n))
+				nbOfTrue++;
+		}
+		System.out.println("There was " + nbOfTrue + " TRUE among " + nbOfTests
+				+ " flips of coin");
+	}
 }
--- src/s13/RndTriangle.java
+++ src/s13/RndTriangle.java
@@ -1,14 +1,31 @@
 package s13;
+
 import java.util.Random;
+import javafx.geometry.Point2D;
 public class RndTriangle {
-  public static void main(String [] args) {
-    int nbOfExperiments = 100000;
-    Random r = new Random();
-    if (args.length>0) nbOfExperiments = Integer.parseInt(args[0]);
-    System.out.println(rndTriangleAvgArea(r, nbOfExperiments));
-  }
-  //============================================================
-  public static double rndTriangleAvgArea(Random r, int nbOfExperiments) {
-    return 0.0; //TODO
-  }
+	public static void main(String[] args) {
+		int nbOfExperiments = 1000000;
+		Random r = new Random();
+		if (args.length > 0)
+			nbOfExperiments = Integer.parseInt(args[0]);
+		System.out.println(rndTriangleAvgArea(r, nbOfExperiments));
+	}
+
+	// ============================================================
+	public static double rndTriangleAvgArea(Random r, int nbOfExperiments) {
+		double rslt = 0;
+		int cmpt = 0;
+		while (cmpt <= nbOfExperiments) {
+			
+			Point2D ptn1 = new Point2D(r.nextDouble(), r.nextDouble());
+			Point2D ptn2 = new Point2D(r.nextDouble(), r.nextDouble());
+			Point2D ptn3 = new Point2D(r.nextDouble(), r.nextDouble());
+			
+			rslt += 0.5 * Math.abs(ptn1.getX() * (ptn2.getY() - ptn3.getY())
+					+ ptn2.getX() * (ptn3.getY() - ptn1.getY()) + ptn3.getX()
+					* (ptn1.getY() - ptn2.getY()));
+			cmpt++;
+		}
+		return rslt / nbOfExperiments;
+	}
 }
