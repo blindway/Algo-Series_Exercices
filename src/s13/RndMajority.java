5e98260492c2e742afba96c50edd991e4284a650
AlexTravasso <alex.travasso@gmail.com>
2015-05-12 16:34:12 +0200

    asdasdsad

---
+++ src/s13/RndMajority.java
@@ -0,0 +1,105 @@
+package s13;
+
+import java.util.Random;
+
+public class RndMajority {
+	public static void main(String[] args) {
+		int nbOfExperiments = 10000;
+		double risk = 1.0 / 1000;
+		Random r = new Random();
+		testMajority(nbOfExperiments, r, risk);
+	}
+
+	// ============================================================
+	public static boolean hasMajority(Random r, int[] t, double risk) {
+		for (int i = 0; i < Math.log(1 / risk); i++) {
+			int val = r.nextInt(t.length);
+			int w = t[val];
+			int cmpt = 0;
+			for (int j = 0; j < t.length; j++) {
+				if (w == t[j]) {
+					cmpt++;
+				}
+			}
+			if (cmpt > t.length / 2) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// ============================================================
+	static final int MAX_VALUE = 1000;
+
+	public static int[] generateMajorityArray(int n, Random r) {
+		// Generate an array that is majoritary with a random value
+		int[] t = new int[n];
+		int m = r.nextInt(MAX_VALUE);
+		// generate array of m
+		for (int i = 0; i < n; i++) {
+			if (i < n / 2 - 1)
+				t[i] = r.nextInt(MAX_VALUE);
+			else
+				t[i] = m;
+		}
+		shuffle(r, t);
+		return t;
+	}
+
+	// ============================================================
+	public static int[] generateNotMajorityArray(int n, Random r) {
+		assert (n > 1);
+		int[] t = new int[n];
+		int m = r.nextInt(MAX_VALUE);
+		int i;
+		t[0] = m - 1;
+		for (i = 1; i < n; i++)
+			if (i - 1 < n / 2)
+				t[i] = m;
+			else
+				t[i] = m + 1;
+		shuffle(r, t);
+		return t;
+	}
+
+	// ============================================================
+	public static void testMajority(int n, Random r, double risk) {
+		int[] t;
+		int mistakes = 0;
+		// test with some majority arrays
+		for (int i = 0; i < n; i++) {
+			t = generateMajorityArray(n, r);
+			if (!hasMajority(r, t, risk))
+				mistakes++;
+		}
+		// test with some not majority arrays
+		for (int i = 0; i < n; i++) {
+			t = generateNotMajorityArray(n, r);
+			if (hasMajority(r, t, risk))
+				throw new RuntimeException(
+						"This array has no majority, but your program has detected it as majority:\n["
+								+ toString(t) + "]");
+		}
+		System.out.println("\nTest passed successfully !");
+		System.out.println("\n Mistake ratio : " + (mistakes / (double) n));
+	}
+
+	// ============================================================
+	static String toString(int[] s) {
+		String str = "";
+		for (int i = 0; i < s.length; i++) {
+			str = str + s[i] + " ";
+		}
+		return str;
+	}
+
+	// ============================================================
+	static void shuffle(Random r, int[] t) {
+		for (int i = 1; i < t.length; i++) {
+			int j = r.nextInt(i + 1);
+			int a = t[i];
+			t[i] = t[j];
+			t[j] = a;
+		}
+	}
+}
