5a390d7e50214c2344e57b02038dea4f8df4b493
blindway <blindway@hotmail.fr>
2015-05-27 16:25:25 +0200

    s15 - debut

--- src/s15/GridPath.java
+++ src/s15/GridPath.java
@@ -1,37 +1,47 @@
 package s15;
+
 public class GridPath {
-  //============================================================
-  // Min Path Problem : Recursive version
-  //============================================================
-  public static int minPath(int [][] t) {
-    return minPath(t, t.length-1, t[0].length-1);
-  }
-  public static int minPath(int [][] t, int i, int j) {
-    if (i <0 || j <0) return Integer.MAX_VALUE/2;
-    if (i==0 && j==0) return t[0][0];
-    int a = minPath(t, i-1, j  );
-    int b = minPath(t, i,   j-1);
-    if (b<a) a=b;
-    return a+t[i][j];
-  }
-  //============================================================
-  // Min Path Problem : Dynamic Programming version
-  //============================================================
-  public static int minPathDyn(int [][] t) {
-    // TODO - A COMPLETER
-    return 0;
-  }
-  //============================================================
-  // Small Main
-  //============================================================
-  public static void main (String [] args) {
-    int [][] t = {
-        {2,2,6,7},
-        {3,8,5,9},
-        {2,1,8,2},
-        {4,2,3,5}
-    };
-    System.out.println(minPath(t));
-    System.out.println(minPathDyn(t));
-  }
+	// ============================================================
+	// Min Path Problem : Recursive version
+	// ============================================================
+	public static int minPath(int[][] t) {
+		return minPath(t, t.length - 1, t[0].length - 1);
+	}
+
+	public static int minPath(int[][] t, int i, int j) {
+		if (i < 0 || j < 0)
+			return Integer.MAX_VALUE / 2;
+		if (i == 0 && j == 0)
+			return t[0][0];
+		int a = minPath(t, i - 1, j);
+		int b = minPath(t, i, j - 1);
+		if (b < a)
+			a = b;
+		return a + t[i][j];
+	}
+
+	// ============================================================
+	// Min Path Problem : Dynamic Programming version
+	// ============================================================
+	public static int minPathDyn(int[][] t) {
+		int n = t.length, m = t[0].length;
+		int[][] minPathSol = new int[n][m];
+		minPathSol[0][0] = t[0][0];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				// TODO finish this shit!!!
+			}
+		}
+		return minPathSol[n - 1][m - 1];
+	}
+
+	// ============================================================
+	// Small Main
+	// ============================================================
+	public static void main(String[] args) {
+		int[][] t = { { 2, 2, 6, 7 }, { 3, 8, 5, 9 }, { 2, 1, 8, 2 },
+				{ 4, 2, 3, 5 } };
+		System.out.println(minPath(t));
+		System.out.println(minPathDyn(t));
+	}
 }
